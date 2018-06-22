abbaf5ff54bdf6643e6a348d79834c4bbdd08755
Christian Edwardson <cedwardson@gmail.com>
2018-06-22 15:11:29 -0500

    Add files via upload

---
+++ phyloseq2QIIME2.R
@@ -0,0 +1,54 @@
+phyloseq2qiime2<-function(physeq){
+  #take a phyloseq object, separate into individual parts, add "Other" OTU row in tax_table, otu_table)
+  library(phyloseq)
+  library(dada2)
+  library(biomformat)
+  library(ape)
+  library(Biostrings)
+  if(packageVersion("biomformat") < "1.7") {
+    stop("This will only work with biomformat version > 1.7")
+  }
+  ps_name <-deparse(substitute(physeq))
+  taxa_are_rows_logical<-taxa_are_rows(physeq)
+  
+  if(is.null(access(physeq,"otu_table"))==FALSE){
+    if(taxa_are_rows_logical==TRUE) {
+      otu<-as(otu_table(physeq),"matrix")
+      otu_biom<-make_biom(data=otu)
+      write_biom(otu_biom,biom_file=paste0(ps_name,"_features-table.biom"))
+      print(paste0("Writing feature table to ",ps_name,"_feature-table.biom"))
+    } else if (taxa_are_rows_logical==FALSE) {
+      otu<-t(as(otu_table(physeq),"matrix"))
+      otu_biom<-make_biom(data=otu)
+      write_biom(otu_biom,biom_file=paste0(ps_name,"_feature-table.biom"))
+      print(paste0("Writing feature table to ",ps_name,"_feature-table.biom"))
+    }
+  }
+  if(is.null(access(physeq,"sam_data"))==FALSE){
+    write.table(sample_data(physeq),file=paste0(ps_name,"_sample-metadata.txt"), 
+                sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
+    print(paste0("Writing sample metadata to ",ps_name,"_sample-metadata.txt"))
+  }
+  if(is.null(access(physeq,"tax_table"))==FALSE){
+    tax<-as(tax_table(physeq),"matrix")
+    tax_cols <- colnames(tax)
+    tax<-as.data.frame(tax)
+    tax$taxonomy<-do.call(paste, c(tax[tax_cols], sep=";"))
+    for(co in tax_cols) tax[co]<-NULL
+    write.table(tax, file=paste0(ps_name,"_tax.txt"), quote=FALSE, col.names=FALSE, sep="\t")
+    print(paste0("Writing taxonomy table to ",ps_name,"_tax.txt"))
+  }
+  if(is.null(access(physeq,"phy_tree"))==FALSE){
+    if(is.rooted(phy_tree(physeq))==TRUE) {
+      ape::write.tree(phy_tree(physeq),file=paste0(ps_name,"_tree-rooted.newick"))
+      print(paste0("Writing rooted tree to ",ps_name,"_tree-rooted.newick"))
+    } else if (is.rooted(phy_tree(physeq))==FALSE) {
+      ape::write.tree(phy_tree(physeq),file=paste0(ps_name,"_tree-unrooted.newick"))
+      print(paste0("Writing unrooted tree to ",ps_name,"_tree-unrooted.newick"))
+    }      
+  }
+  if(is.null(access(physeq,"refseq"))==FALSE){
+    writeXStringSet(refseq(physeq),filepath=paste0(ps_name,"_ref-seqs.fasta"))
+    print(paste0("Writing reference sequences to FASTA file ",ps_name,"_ref-seqs.fasta"))
+  } 
+}
