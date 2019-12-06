9b31c70dd372ac759b4dae2755ae8cab8a379312
Christian Edwardson <cedwardson@gmail.com>
2019-12-06 15:55:04 -0500

    Fix errors
    - fix hard-coded 100 trial for loop (1:100 --> 1:trials)
    - change X.SampleID requirement (qiime1 imported metadata) to SampleID, merge with metadata on column1
    - remove project specific commented line

--- AlphaDiversityFunction.R
+++ AlphaDiversityFunction.R
@@ -16,7 +16,7 @@ alphaDivSubsample<-function(ps,richness_measure="Observed",evenness_measure="Inv
   row.names(evenness) <- sample_names(ps)
   set.seed(100)
 
-  for (i in 1:100) {
+  for (i in 1:trials) {
     r <- rarefy_even_depth(ps, sample.size = min_lib, verbose = FALSE, replace = TRUE)
   
     # Calculate richness
@@ -29,25 +29,24 @@ alphaDivSubsample<-function(ps,richness_measure="Observed",evenness_measure="Inv
   }
 
 # Create a new dataframe to hold the means and standard deviations of richness estimates
-  X.SampleID <- row.names(richness)
+  SampleID <- row.names(richness)
   mean <- apply(richness, 1, mean)
   sd <- apply(richness, 1, sd)
   measure <- rep("Richness", nsamp)
-  rich_stats <- data.frame(X.SampleID, mean, sd, measure)
+  rich_stats <- data.frame(SampleID, mean, sd, measure)
 
   # Create a new dataframe to hold the means and standard deviations of evenness estimates
-  X.SampleID <- row.names(evenness)
+  SampleID <- row.names(evenness)
   mean <- apply(evenness, 1, mean)
   sd <- apply(evenness, 1, sd)
   measure <- rep("Inverse Simpson", nsamp)
-  even_stats <- data.frame(X.SampleID, mean, sd, measure)
+  even_stats <- data.frame(SampleID, mean, sd, measure)
 
 
   alpha <- rbind(rich_stats, even_stats)
 
   sample_data_df <- data.frame(sample_data(ps))
-  #AQComp_sample_df<-plyr::rename(AQComp_sample_df, replace=c("X.SampleID"="X.SampleID"))
-  alphadiv <- merge(alpha, sample_data_df, by = "X.SampleID")
+  alphadiv <- merge(alpha, sample_data_df, by = 1)
   return(alphadiv)
 }
 
